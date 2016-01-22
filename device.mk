983482abc91f60d2850854402397f69aba0c7bb9
Tim Klein <cuzz1369@gmail.com>
2016-01-22 14:58:36 -0500

    All CPU's for boot

--- device.mk
+++ device.mk
@@ -359,8 +359,8 @@ PRODUCT_PACKAGES += \
 
 # limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-threads=2 \
-    dalvik.vm.image-dex2oat-threads=4
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-threads=8
 
 # Modem debugger
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
