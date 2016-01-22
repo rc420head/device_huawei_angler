e6c9a2fd76a0d7d7708a35d24d35690f2da3bb5b
Paul Keith <javelinanddart@gmail.com>
2016-01-22 16:02:50 -0600

    angler: Use UBER GCC 5.3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,10 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := cyanogenmod_angler_defconfig
 
+# Uber GCC 5.3 Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/UBERTC-aarch64-linux-android-5.3-kernel/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
